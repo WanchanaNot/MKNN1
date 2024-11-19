c9d144a92fe10f1fe9c1074cacbc6e426b1e0cf5
WanchanaNot <aoyzaza33317@gmail.com>
2024-11-19 16:10:02 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-st.header('Wanchana Laoeiam')
+st.header('🗽Wanchana Laoeiam⚽️')
 st.image("./img/Wanchana.jpg")
 col1, col2, col3 = st.columns(3)
 
