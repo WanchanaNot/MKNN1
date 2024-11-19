20fd6bb3cb49968935d59434a04d39defbbfe94a
WanchanaNot <aoyzaza33317@gmail.com>
2024-11-19 14:42:27 +0700

    .

--- home.py
+++ home.py
@@ -4,4 +4,9 @@ import pandas as pd
 st.title("Website Developing using Python")
 st.header("Website Developing using Python")
 
+st.image('./img/Me.jpg')
 st.subheader("Wanchana Laoeiam")
+
+dt=pd.read_csv('./data/iris-3.csv')
+st.header()
+st.write(dt.head(10))
